b53f4f1279fd84d91d0a67955eeacadcf05c3f08
Adhish Thite <adhish.thite@elastic.co>
2025-03-04 13:49:27 +0530

    cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,28 +78,8 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+      keyframes: {},
+      animation: {},
       fontSize: {
         xs: ['0.8125rem', { lineHeight: '1.5' }],
         sm: ['0.9375rem', { lineHeight: '1.5' }],
